98b168a5cdfbe78841d27f76360717724a289df8
Tushar Patil <71126405+tushasrspatil5@users.noreply.github.com>
2022-09-30 14:46:27 +0530

    added all files

---
+++ backend/app.py
@@ -0,0 +1,91 @@
+import json
+from urllib import response
+from flask import Flask, render_template, request,make_response
+from flask_pymongo import PyMongo
+from flask_cors import CORS, cross_origin
+from bson import json_util
+import datetime
+from flask import Flask, request, jsonify
+from flask_mongoengine import MongoEngine
+
+#initialize the Flask app
+app = Flask("myapp")
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'test',
+    'host': 'localhost',
+    'port': 27017
+}
+db = MongoEngine()
+
+db.init_app(app)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+
+@app.route("/get_data")
+def hello():
+   json_data ={'name':'tushar'}
+   return json_data
+
+
+class ScraperTable(db.Document):
+    custom_id =db.IntField()
+    name = db.StringField()
+    price = db.StringField()
+    h_1 = db.StringField()
+    h_24 = db.StringField()
+    d_7 = db.StringField()
+    market_cap = db.StringField()
+    volume = db.StringField()
+    circulating_Supply = db.StringField()
+    meta = {'strict': False}
+
+
+@app.route('/fetch-data', methods=['GET'])
+def query_records():
+    user = ScraperTable.objects.all()
+    if not user:
+        return jsonify({'error': 'data not found'})
+    else:
+        return jsonify(user)
+
+
+
+@app.route('/update-record', methods=['POST'])
+def update_record():
+    post_data = request.json
+    for d_list in post_data:
+
+        user_id = ScraperTable.objects.filter(custom_id=d_list['id']).first()
+        if not user_id:
+            new_instance = ScraperTable(
+                custom_id=d_list['id'],
+                name=d_list['name'],
+                price=d_list['price'],
+                h_1=d_list['h_1'],
+                h_24=d_list['h_24'],
+                d_7=d_list['d_7'],
+                market_cap=d_list['market_cap'],
+                volume=d_list['volume'],
+                circulating_Supply=d_list['circulating_Supply'])
+            new_instance.save()
+        else:
+            user_id.update(name=d_list['name'],
+                price=d_list['price'],
+                h_1=d_list['h_1'],
+                h_24=d_list['h_24'],
+                d_7=d_list['d_7'],
+                market_cap=d_list['market_cap'],
+                volume=d_list['volume'],
+                circulating_Supply=d_list['circulating_Supply'])
+    return jsonify(user_id.to_json())
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
+
---
+++ scraper.py
@@ -0,0 +1,81 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+import time
+
+import os
+driverpath = os.path.abspath(os.getcwd())+"/chromedriver.exe"
+s=Service(executable_path= str(driverpath))
+import requests
+
+class App:
+    def __init__(self):
+        # selenium chrome driver configuration
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-web-security")
+        chrome_options.add_argument("--allow-running-insecure-content")
+        chrome_options.add_argument("--log-level=3")
+        chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2,'profile.default_content_settings.images':2})
+        self.driver = webdriver.Chrome(service = s,options=chrome_options)
+    
+    def scrap(self,link)-> None:
+        while(True):
+            self.driver.get(link)
+            sleep(3)
+            try:
+                self.driver.find_element(By.CLASS_NAME,"css-1pcqseb").click()
+            except:
+                pass
+            page = 1
+            count_id = 0
+            scrapped_data = []
+            elements = self.driver.find_elements_by_xpath("//table[@class='h7vnx2-2 juYUEZ cmc-table  ']/tbody/tr")
+            for element in elements:
+                count_id = count_id + 1
+                try:
+                    name= element.find_element_by_xpath("./td/div/a/div/div/p[@class='sc-14rfo7b-0 lhJnKD']")
+                    price = element.find_element_by_xpath("./td/div[@class='sc-131di3y-0 cLgOOr']")
+                    row = element.find_elements_by_xpath("./td/span[contains(@class,'sc-15yy2pl-0 hzgCfk') or contains(@class,'sc-15yy2pl-0 kAXKAX')]")
+                    h_1 = row[0].text,
+                    h_24 = row[1].text,
+                    d_7 = row[2].text
+                    market_cap = element.find_element_by_xpath("./td/p[@class='sc-14rfo7b-0 fVSMmK'] ").text
+                    volume = element.find_element_by_xpath("./td/div[@class='sc-1prm8qw-0 j3nwcd-0 kLJESq'] /a").text
+                    circulating_Supply = element.find_element_by_xpath("./td/div[@class='sc-1prm8qw-0 sc-1gslw1d-0 jHWhpv']/div").text
+                    
+                    data_dict= {
+                        'id':count_id,
+                        "name":name.text,
+                        "price":price.text,
+                        "h_1":h_1[0],
+                        "h_24":h_24[0],
+                        "d_7":d_7,
+                        "market_cap":market_cap,
+                        "volume":volume,
+                        "circulating_Supply":circulating_Supply  
+                    }
+                    scrapped_data.append(data_dict)
+
+                except:
+                    count_id = count_id-1
+                    pass
+            
+
+            url = "http://127.0.0.1:5000/update-record"
+            data = scrapped_data
+            response = requests.post(url, json=data)
+            print("Status Code", response.status_code)
+            # time.sleep(300)
+
+        def close(self)-> None:
+            self.driver.close()
+        
+        
+
+a = App()
+a.scrap("https://coinmarketcap.com")
+a.close()
+
